1d1424ba8873b18705a99cbf1b37b3f3dc8ef90a
smkhairkar <85014392+smkhairkar@users.noreply.github.com>
2024-10-19 10:32:00 +0000

    vaiables

---
+++ 01_basics/variables.js
@@ -0,0 +1,23 @@
+  const accountID = 1234
+  let accountEmail = "sujata.khairkar@gmail.com"
+  var accountpassword = "1234"
+  accountcity = "Ahemdabad"
+
+
+  //accountID = 2
+
+  accountEmail = "khairkar.sujata@gmail.com"
+  accountpassword = "23456"
+  accountcity = "Amerli"
+
+  console.log(accountID);
+
+  console.table([accountID, accountEmail, accountpassword, accountcity])
+
+
+
+// Prefer not to use VAR
+// because of the issues in block scope and functional scope
+
+
+
