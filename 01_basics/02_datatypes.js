60cbb18c8942a00db70ce3105e6ddc75b9ff10b8
smkhairkar <85014392+smkhairkar@users.noreply.github.com>
2024-10-22 07:37:33 +0000

    conversionoperation

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,20 @@
+ console.log('Sujata Khairkar');
+let age = 18
+let isloggedin = false
+let state;
+
+
+//number => 2 to power 34
+//bigint
+//string => ""
+//boolean
+//null => standalone value
+//undefined
+//symbol
+
+//object
+
+console.log(typeof age);
+console.log(typeof null);//object
+console.log(typeof undefined) //undefined
+
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,18 @@
+// let score = '33';
+
+// console.log(typeof score);
+
+// let valueinNumber = Number(score);
+// console.log(typeof valueinNumber);
+// console.log(valueinNumber);
+
+
+//"33" => 33
+//"33abc" => NaN
+// true =>1; false =>0
+
+let isLoggedIn = 1;
+
+let booleanisLoggedIn = Boolean(isLoggedIn);
+console.log(booleanisLoggedIn);
+
