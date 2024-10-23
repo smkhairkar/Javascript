0381efb168fa4b88d9296f984ff65c70bae8a56d
smkhairkar <85014392+smkhairkar@users.noreply.github.com>
2024-10-23 11:25:12 +0000

    comparison

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,26 @@
+// console.log(2>1);
+// console.log(2>=1);
+// console.log(2<1);
+// console.log(2==1);
+// console.log(2!=1);
+
+// console.log("2">1);
+// console.log("-2">1);
+
+//typescript doesn't allow you to compare two different data types 
+
+console.log(null >= 0);
+console.log(null == 0);
+console.log(null > 0);
+
+console.log(undefined == 0);
+console.log(undefined >0);
+console.log(undefined<0);
+
+
+
+console.log("2" == 2);
+
+
+
+
