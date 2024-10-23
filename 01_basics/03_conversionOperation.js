449b324053333a3fd7bde07fc65c9cc13562e286
smkhairkar <85014392+smkhairkar@users.noreply.github.com>
2024-10-23 07:16:31 +0000

    conversionopration_1

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -16,3 +16,50 @@ let isLoggedIn = 1;
 let booleanisLoggedIn = Boolean(isLoggedIn);
 console.log(booleanisLoggedIn);
 
+// ***************operations******************
+let value = 3
+let negvalue = -value
+
+console.log(negvalue);
+
+// console.log(2+2);
+
+// console.log(2-2);
+
+// console.log(2*2);
+
+// console.log(2**2);
+
+// console.log(2/2);
+
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = "sujata"
+
+let str3 = str1 + " "+ str2
+console.log(str3);
+
+
+//problems might arise in the following cases:-
+
+console.log("1" + 2)
+
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
