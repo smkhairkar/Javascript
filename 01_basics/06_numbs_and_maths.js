ed6f1092ab28c9bb27f43ca492537c68d043844c
smkhairkar <85014392+smkhairkar@users.noreply.github.com>
2024-11-11 11:24:18 +0000

    math&num

---
+++ 01_basics/06_numbs_and_maths.js
@@ -0,0 +1,29 @@
+const score = 500;
+console.log(score);
+const balance = new Number(100)
+console.log(balance);
+
+console.log(balance.toString().length);
+console.log(balance.toFixed(1));
+
+console.log(typeof(balance));
+
+const num = 25.9454336
+
+console.log(num.toPrecision(3));
+
+const hundreds = 100000000
+
+console.log(hundreds.toLocaleString('en-IN'));
+
+// =====================MATHS=====================
+console.log(Math);
+console.log(Math.abs(-999));
+console.log(Math.round(7.5));
+console.log(Math.ceil(4.8));
+console.log(Math.floor(4.8));
+console.log(Math.max(2,893,57483,23434));
+console.log(Math.min(238487, 24343,32455));
+
+console.log(Math.random());
+console.log(Math.random()* 10 + 1); 
