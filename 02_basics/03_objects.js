8cbff2d7c4c3f0a14d6d1ef0064b940c3d431644
smkhairkar <85014392+smkhairkar@users.noreply.github.com>
2024-11-22 11:32:59 +0000

    object

---
+++ 02_basics/03_objects.js
@@ -0,0 +1,45 @@
+//singleton
+
+// object literals
+
+// Object.create
+
+const mySym = Symbol("key1")
+
+
+const JSUser = {
+    name: "Sujata",
+    "full name": "Sujata Khairkar",
+    age: 23,
+    [mySym] : "key1",
+    location: "Ahemdabad",
+    email: "sujata@gmail.com",
+    isLoggedIn :false,
+    lastLoginDays : ["Monday", "Saturday"]
+}
+//how to access:-
+console.log(JSUser.email);
+console.log(JSUser["email"]);
+console.log(JSUser["full name"]);
+//console.log(JSUser.mySym); ===WRONG
+
+console.log(JSUser[mySym])
+
+//overwrite
+JSUser.email = "sujata@chatgpt.com"
+//Object.freeze(JSUser)
+JSUser,email="sujata@chatgpt.com"
+
+console.log(JSUser);
+
+JSUser.greeting = function(){
+    console.log("Hello JS User");
+
+}
+
+JSUser.greetingTwo = function(){
+    console.log(`Hello JS user, ${this.name}`);
+
+}
+console.log(JSUser.greeting());
+console.log(JSUser.greetingTwo());
